d370e3cfe467d5d010b11ab471fd6a4826f46e7f
Hagen Carstensen <hagen.carstensen@porsche.de>
2021-07-16 14:30:21 +0200

    solve 3.6

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -27,8 +28,8 @@ let persons = [
 ]
 
 const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
         : 0
     return maxId + 1
 }
@@ -36,19 +37,29 @@ const generateId = () => {
 app.post('/api/persons', (req, res) => {
     const body = req.body
 
-    if (!body.content) {
+    
+    if (!body.name) {
         return res.status(400).json({
-            error: 'content missing'
+            error: 'body is missing name'
         })
     }
-
+    if (!body.number) {
+        return res.status(400).json({
+            error: 'body is missing number'
+        })
+    }
+    if (persons.map(person => person.name).includes(body.name)) {
+        return res.status(400).json({
+            error: 'Person already in your phonebook'
+        })
+    }
+    console.log(req.body);
     const person = {
-        content: body.content,
         id: generateId(),
-        name: body.name || false,
+        name: body.name ,
         number: body.number,
     }
-    persons = persons.content(person)
+    persons = persons.concat(person)
 
     res.status(201).json(person)
 })
@@ -57,15 +68,29 @@ app.get('/api/persons/', (req, res) => {
 })
 app.get('/info', (req, res) => {
     let date = new Date();
+    //I hate myself for doing that
     let str = "<div>"+"<p>"+'Phonebook has info for '+persons.length+' people'+"</p>"+"<p>"+date.toUTCString()+"</p>"+"</div>"
-    var html = `
-    <div>
-        <p>Phonebook has info for {persons.length} people</p>
-        <p>{Date.now()}</p>
-    </div>`
     res.status(200).send(str)
     
 })
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    person = persons.filter(person => person.id === id)
+    if(person){
+        res.status(200).json(person)
+    }else{
+        res.status(404).send('Error: Person does not exist')
+    }
+    
+})
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+    
+})
+
 const PORT = 3002
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
