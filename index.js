670b096c232a2a11558796d231a9766f3b2bee57
Hagen Carstensen <hagen.carstensen@porsche.de>
2021-07-28 09:40:00 +0200

    solve 3.15-3.18 for real

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/api/persons', (req, res) => {
 
 })
 app.put('/api/persons/:id', (req, res) => {
-   Person.findByIdAndUpdate(req.params.id, req.body,options.new=true)
+   Person.findByIdAndUpdate(req.params.id, req.body,{new: true})
    .then(updated => {
        res.json(updated)
    })
